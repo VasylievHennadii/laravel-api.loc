c7c504544681963bf751a284f75e27c1ffa96bac
Vasyliev Hennadii <vga.kha@gmail.com>
2025-06-08 12:11:52 +0200

    Create Category. Test API request.

--- app/Http/Controllers/Api/V1/CategoryController.php
+++ app/Http/Controllers/Api/V1/CategoryController.php
@@ -25,7 +25,7 @@ class CategoryController extends Controller
      */
     public function store(StoreCategoryRequest $request)
     {
-        //
+        return Category::create($request->all());
     }
 
     /**
