222a6768ee691c0a2e1603e5c2672b782337432e
Vasyliev Hennadii <vga.kha@gmail.com>
2025-06-25 22:30:13 +0200

    Pagination in api response format using PostResource.

--- app/Http/Controllers/Api/V1/PostController.php
+++ app/Http/Controllers/Api/V1/PostController.php
@@ -15,7 +15,7 @@ class PostController extends Controller
      */
     public function index()
     {
-        return PostResource::collection(Post::with('category')->get());
+        return PostResource::collection(Post::with('category')->paginate(5));
     }
 
     /**
