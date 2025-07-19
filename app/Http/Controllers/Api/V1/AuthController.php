593303dbd44118c6316063ac5ab0e25c83a5a8e3
Vasyliev Hennadii <vga.kha@gmail.com>
2025-07-19 12:10:47 +0200

    Authentication: Creating a set of methods for AuthController.

--- app/Http/Controllers/Api/V1/AuthController.php
+++ app/Http/Controllers/Api/V1/AuthController.php
@@ -3,9 +3,24 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginUserRequest;
+use App\Http\Requests\StoreUserRequest;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    //
+    public function register(StoreUserRequest $request)
+    {
+        return 'Register';
+    }
+
+    public function login(LoginUserRequest $request)
+    {
+        return 'Login';
+    }
+
+    public function logout()
+    {
+        return 'Logout';
+    }
 }
