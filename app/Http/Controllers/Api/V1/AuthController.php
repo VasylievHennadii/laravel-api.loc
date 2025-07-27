f8592e3a29a642867e4c7a68f4bf17a60e5c2c82
Vasyliev Hennadii <vga.kha@gmail.com>
2025-07-27 12:49:25 +0200

    Authentication: User registration.

--- app/Http/Controllers/Api/V1/AuthController.php
+++ app/Http/Controllers/Api/V1/AuthController.php
@@ -5,13 +5,14 @@ namespace App\Http\Controllers\Api\V1;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\LoginUserRequest;
 use App\Http\Requests\StoreUserRequest;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
     public function register(StoreUserRequest $request)
     {
-        return 'Register';
+        return User::create($request->all());
     }
 
     public function login(LoginUserRequest $request)
